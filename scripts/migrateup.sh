62ad770eb182ec71842a8cca4a1524377d2c80ca
lsherman98 <122409681+lsherman98@users.noreply.github.com>
2024-08-12 23:48:41 -0400

    absolute paths for migrate script

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -4,5 +4,6 @@ if [ -f .env ]; then
     source .env
 fi
 
-cd sql/schema
+[ -f "$(dirname "$0")/../.env" ] && source "$(dirname "$0")/../.env"
+cd "$(dirname "$0")/../sql/schema" || exit
 goose turso $DATABASE_URL up
